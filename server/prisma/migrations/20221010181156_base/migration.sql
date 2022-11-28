d9da59bebfe02d28ff356f605b2edde0e436f67c
Samuel Sena <samuelcardososena19@gmail.com>
2022-11-28 00:34:51 -0300

    fixed 3.0

--- server/prisma/migrations/20221010181156_base/migration.sql
+++
@@ -1,7 +0,0 @@
--- Active: 1666494148829@@127.0.0.1@3306
--- CreateTable
-CREATE TABLE "Cliente" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "bannerUrl" TEXT NOT NULL
-);
